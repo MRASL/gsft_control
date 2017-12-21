f8cd5188331295ab609bb34b028c8f7b2733d7ca
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2017-12-20 19:21:04 -0500

    full tess

---
+++ src/lqr_full_grt_rtw/rtwtypes.h
@@ -0,0 +1,42 @@
+/*
+ * rtwtypes.h
+ *
+ * Student License - for use by students to meet course requirements and
+ * perform academic research at degree granting institutions only.  Not
+ * for government, commercial, or other organizational use.
+ *
+ * Code generation for model "lqr_full".
+ *
+ * Model version              : 1.471
+ * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
+ * C++ source code generated on : Wed Dec 20 19:12:49 2017
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objective: Execution efficiency
+ * Validation result: Passed (1), Warnings (3), Error (0)
+ */
+
+#ifndef RTWTYPES_H
+#define RTWTYPES_H
+#include "tmwtypes.h"
+#include "simstruc_types.h"
+#ifndef POINTER_T
+# define POINTER_T
+
+typedef void * pointer_T;
+
+#endif
+
+/* Logical type definitions */
+#if (!defined(__cplusplus))
+#  ifndef false
+#   define false                       (0U)
+#  endif
+
+#  ifndef true
+#   define true                        (1U)
+#  endif
+#endif
+#endif                                 /* RTWTYPES_H */
