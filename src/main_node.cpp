d6c818ebf5714a5b8f5b230bec16b3571aa83ad1
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-04 14:43:33 -0500

    dyn reconfig

--- src/main_node.cpp
+++ src/main_node.cpp
@@ -1,20 +1,18 @@
 #include <ros/ros.h>
 
 #include <dynamic_reconfigure/server.h>
-#include <gsft_control/ControllerDynConfig.h>
+#include <gsft_control/controllerDynConfig.h>
 
-void controller_dyn_callback(gsft_control::ControllerDynConfig &config, uint32_t level) {
-//  ROS_INFO("Reconfigure Request: %s",
-//            config.start_controller?"True":"False");
-  ROS_INFO("Reconfigure Request: %d ",
-            config.int_param);
+void controller_dyn_callback(gsft_control::controllerDynConfig &config, uint32_t level) {
+  ROS_INFO("Reconfigure Request: %s",
+            config.start_controller?"True":"False");
 }
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "main_node");
 
-  dynamic_reconfigure::Server<gsft_control::ControllerDynConfig> server;
-  dynamic_reconfigure::Server<gsft_control::ControllerDynConfig>::CallbackType f;
+  dynamic_reconfigure::Server<gsft_control::controllerDynConfig> server;
+  dynamic_reconfigure::Server<gsft_control::controllerDynConfig>::CallbackType f;
 
   f = boost::bind(&controller_dyn_callback, _1, _2);
   server.setCallback(f);
