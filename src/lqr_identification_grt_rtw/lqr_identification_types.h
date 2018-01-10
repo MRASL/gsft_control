d38f7b4975d4aef6bfa5108bf35e1b4f85ce5853
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-10 09:05:08 -0500

    first test today

--- src/lqr_identification_grt_rtw/lqr_identification_types.h
+++ src/lqr_identification_grt_rtw/lqr_identification_types.h
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.526
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Tue Jan  9 17:24:19 2018
+ * C++ source code generated on : Wed Jan 10 09:03:02 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
