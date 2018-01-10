4f9afc5b069770c6eee91458e9a65d7ffbe13ad9
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-10 18:42:29 -0500

    file

---
+++ src/lqr_inner_grt_rtw/lqr_inner_data.cpp
@@ -0,0 +1,56 @@
+/*
+ * lqr_inner_data.cpp
+ *
+ * Student License - for use by students to meet course requirements and
+ * perform academic research at degree granting institutions only.  Not
+ * for government, commercial, or other organizational use.
+ *
+ * Code generation for model "lqr_inner".
+ *
+ * Model version              : 1.530
+ * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
+ * C++ source code generated on : Wed Jan 10 18:26:04 2018
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objective: Execution efficiency
+ * Validation result: Passed (1), Warnings (3), Error (0)
+ */
+
+#include "lqr_inner.h"
+#include "lqr_inner_private.h"
+
+/* Constant parameters (auto storage) */
+const ConstP_lqr_inner_T lqr_inner_ConstP = {
+  /* Expression: [mass*gra 0 0 0]'
+   * Referenced by: '<Root>/              '
+   */
+  { 14.679084052499999, 0.0, 0.0, 0.0 },
+
+  /* Expression: Ki_lqr
+   * Referenced by: '<Root>/ '
+   */
+  { 1.0000000000000002, -0.0, -0.0, -0.0, -0.0, 0.99999999999999423, -0.0, -0.0,
+    -0.0, -0.0, 1.000000000000006, -0.0, -0.0, -0.0, -0.0, 1.0 },
+
+  /* Expression: Kx_lqr
+   * Referenced by: '<Root>/                   '
+   */
+  { 2.6395562446770739, 0.0, 0.0, 0.0, 2.9836285844068722, 0.0, 0.0, 0.0, 0.0,
+    1.7661300127234885, 0.0, 0.0, 0.0, 0.0, 1.7767740923821127, 0.0, 0.0, 0.0,
+    0.0, 1.8246676386533587, 0.0, 1.0596076109213444, 0.0, 0.0, 0.0, 0.0,
+    1.0784630876801333, 0.0, 0.0, 0.0, 0.0, 1.1647059957744119 },
+
+  /* Expression: B_ENU_inv
+   * Referenced by: '<Root>/Gain'
+   */
+  { 0.16666666666666674, 0.16666666666666669, 0.16666666666666671,
+    0.16666666666666671, 0.16666666666666674, 0.16666666666666671,
+    0.77519379844961245, 1.5503875968992242, 0.77519379844961234,
+    -0.775193798449612, -1.5503875968992249, -0.77519379844961223,
+    -1.3426750446270366, 7.0851742975913985E-16, 1.3426750446270364,
+    1.3426750446270364, -4.8647282483410855E-16, -1.3426750446270366,
+    -10.416666666666663, 10.416666666666663, -10.416666666666655,
+    10.416666666666657, -10.416666666666661, 10.416666666666661 }
+};
