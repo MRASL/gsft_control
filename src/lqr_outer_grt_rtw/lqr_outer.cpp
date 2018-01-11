39c9aa8cea43370491e186c55e2b0e5bb0aead0a
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-11 15:51:39 -0500

    new

--- src/lqr_outer_grt_rtw/lqr_outer.cpp
+++ src/lqr_outer_grt_rtw/lqr_outer.cpp
@@ -7,9 +7,9 @@
  *
  * Code generation for model "lqr_outer".
  *
- * Model version              : 1.537
+ * Model version              : 1.538
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Thu Jan 11 15:10:40 2018
+ * C++ source code generated on : Thu Jan 11 15:47:24 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
@@ -326,7 +326,7 @@ void lqr_outerModelClass::step()
     /* '<S2>:1:27' psi = 0; */
   }
 
-  /* Outport: '<Root>/ref' incorporates:
+  /* Outport: '<Root>/x_ref' incorporates:
    *  MATLAB Function: '<Root>/MATLAB Function'
    */
   /*  if t <= 30 */
@@ -341,10 +341,18 @@ void lqr_outerModelClass::step()
   /*      psi = 0; */
   /*  end */
   /* '<S2>:1:40' ref = [x;y;z;psi]; */
-  lqr_outer_Y.ref[0] = x;
-  lqr_outer_Y.ref[1] = 0.0;
-  lqr_outer_Y.ref[2] = rtb_Clock;
-  lqr_outer_Y.ref[3] = 0.0;
+  lqr_outer_Y.x_ref = x;
+
+  /* Outport: '<Root>/y_ref' */
+  lqr_outer_Y.y_ref = 0.0;
+
+  /* Outport: '<Root>/z_ref' incorporates:
+   *  MATLAB Function: '<Root>/MATLAB Function'
+   */
+  lqr_outer_Y.z_ref = rtb_Clock;
+
+  /* Outport: '<Root>/psi_ref' */
+  lqr_outer_Y.psi_ref = 0.0;
 
   /* Sum: '<S1>/Sum4' incorporates:
    *  Inport: '<Root>/X'
@@ -355,9 +363,9 @@ void lqr_outerModelClass::step()
    *  Integrator: '<S3>/Integrator'
    *  Sum: '<S3>/Sum'
    */
-  tmp_1[0] = -0.0 * lqr_outer_X.Integrator_CSTATE_b[0] + -0.0044721359549995772 *
+  tmp_1[0] = -0.0 * lqr_outer_X.Integrator_CSTATE_b[0] + -0.031622776601683777 *
     lqr_outer_X.Integrator_CSTATE_b[1];
-  tmp_1[1] = 0.0044721359549995928 * lqr_outer_X.Integrator_CSTATE_b[0];
+  tmp_1[1] = 0.031622776601683868 * lqr_outer_X.Integrator_CSTATE_b[0];
   tmp_1[1] += -0.0 * lqr_outer_X.Integrator_CSTATE_b[1];
   for (i = 0; i < 2; i++) {
     /* Gain: '<S3>/                   ' incorporates:
--- src/lqr_outer_grt_rtw/lqr_outer.h
+++ src/lqr_outer_grt_rtw/lqr_outer.h
@@ -7,9 +7,9 @@
  *
  * Code generation for model "lqr_outer".
  *
- * Model version              : 1.537
+ * Model version              : 1.538
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Thu Jan 11 15:10:40 2018
+ * C++ source code generated on : Thu Jan 11 15:47:24 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
@@ -225,7 +225,10 @@ typedef struct {
   real_T virtual_control[4];           /* '<Root>/virtual_control' */
   real_T motor_speed[6];               /* '<Root>/motor_speed' */
   real_T motor_RPM[6];                 /* '<Root>/motor_RPM' */
-  real_T ref[4];                       /* '<Root>/ref' */
+  real_T x_ref;                        /* '<Root>/x_ref' */
+  real_T y_ref;                        /* '<Root>/y_ref' */
+  real_T z_ref;                        /* '<Root>/z_ref' */
+  real_T psi_ref;                      /* '<Root>/psi_ref' */
 } ExtY_lqr_outer_T;
 
 /* Real-time Model Data Structure */
