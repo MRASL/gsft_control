9fb54c68215283fbc876886e94a9587a9b74db3f
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-11 10:36:18 -0500

    FASTER ATTITUDE

--- src/lqr_inner_grt_rtw/lqr_inner.h
+++ src/lqr_inner_grt_rtw/lqr_inner.h
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.531
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Thu Jan 11 10:19:18 2018
+ * C++ source code generated on : Thu Jan 11 10:35:51 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
@@ -183,7 +183,7 @@ typedef struct {
 
 /* Constant parameters (auto storage) */
 typedef struct {
-  /* Expression: [mass*gra+5 0 0 0]'
+  /* Expression: [mass*gra+2 0 0 0]'
    * Referenced by: '<Root>/              '
    */
   real_T _Value[4];
