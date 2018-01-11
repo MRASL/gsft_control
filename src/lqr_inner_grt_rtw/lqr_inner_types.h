66a87e8d5ac868747ce74a63f6ea5c56797b00d5
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-10 19:06:36 -0500

    filedd

--- src/lqr_inner_grt_rtw/lqr_inner_types.h
+++ src/lqr_inner_grt_rtw/lqr_inner_types.h
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.530
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Wed Jan 10 18:54:22 2018
+ * C++ source code generated on : Wed Jan 10 19:06:18 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
