73d705f1f4e57d42ab169f71f0f9333b07c1f7c6
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-15 10:49:05 -0500

    yaw

--- src/lqr_hovering_grt_rtw/lqr_hovering.cpp
+++ src/lqr_hovering_grt_rtw/lqr_hovering.cpp
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.518
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Mon Jan 15 08:38:32 2018
+ * C++ source code generated on : Mon Jan 15 10:48:25 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
@@ -402,9 +402,9 @@ void lqr_hoveringModelClass::step()
       ->solverInfo);
 
     {
-      /* Update absolute timer for sample time: [0.01s, 0.0s] */
+      /* Update absolute timer for sample time: [0.016666666666666666s, 0.0s] */
       /* The "clockTick1" counts the number of times the code of this task has
-       * been executed. The resolution of this integer timer is 0.01, which is the step size
+       * been executed. The resolution of this integer timer is 0.016666666666666666, which is the step size
        * of the task. Size of "clockTick1" ensures timer will not overflow during the
        * application lifespan selected.
        * Timer of this task consists of two 32 bit unsigned integers.
@@ -477,7 +477,7 @@ void lqr_hoveringModelClass::initialize()
                     ->intgData);
   rtsiSetSolverName(&(&lqr_hovering_M)->solverInfo,"ode5");
   rtmSetTPtr(getRTM(), &(&lqr_hovering_M)->Timing.tArray[0]);
-  (&lqr_hovering_M)->Timing.stepSize0 = 0.01;
+  (&lqr_hovering_M)->Timing.stepSize0 = 0.016666666666666666;
 
   /* block I/O */
   (void) memset(((void *) &lqr_hovering_B), 0,
--- src/lqr_hovering_grt_rtw/multiword_types.h
+++ src/lqr_hovering_grt_rtw/multiword_types.h
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.518
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Mon Jan 15 08:38:32 2018
+ * C++ source code generated on : Mon Jan 15 10:48:25 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
