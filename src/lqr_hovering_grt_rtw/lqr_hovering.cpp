f4ca19196055b143b9d09dae0c89ac7630bee117
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-15 11:28:14 -0500

    return param

--- src/lqr_hovering_grt_rtw/lqr_hovering.cpp
+++ src/lqr_hovering_grt_rtw/lqr_hovering.cpp
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.519
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Mon Jan 15 11:19:37 2018
+ * C++ source code generated on : Mon Jan 15 11:27:49 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
@@ -231,7 +231,7 @@ void lqr_hoveringModelClass::step()
     /* Sqrt: '<Root>/Sqrt' incorporates:
      *  Gain: '<Root>/Gain2'
      */
-    psi = std::sqrt(168290.67164807054 * rtb_Clock);
+    psi = std::sqrt(116978.4923343994 * rtb_Clock);
 
     /* Gain: '<Root>/rads_to_RPM' */
     rtb_rads_to_RPM = 9.5493 * psi;
