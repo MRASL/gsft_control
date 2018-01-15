f6465152cfbe7738bd4f0cb0087c36b8f5d42e16
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-15 11:47:08 -0500

    yaw

--- src/lqr_hovering_grt_rtw/lqr_hovering.cpp
+++ src/lqr_hovering_grt_rtw/lqr_hovering.cpp
@@ -7,9 +7,9 @@
  *
  * Code generation for model "lqr_hovering".
  *
- * Model version              : 1.519
+ * Model version              : 1.520
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Mon Jan 15 11:27:49 2018
+ * C++ source code generated on : Mon Jan 15 11:46:46 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
