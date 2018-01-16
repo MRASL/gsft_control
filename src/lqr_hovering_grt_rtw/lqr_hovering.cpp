605e65fe535efc4e849f25fcb6d358365dc11c43
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-16 17:11:25 -0500

    aa

--- src/lqr_hovering_grt_rtw/lqr_hovering.cpp
+++ src/lqr_hovering_grt_rtw/lqr_hovering.cpp
@@ -7,9 +7,9 @@
  *
  * Code generation for model "lqr_hovering".
  *
- * Model version              : 1.526
+ * Model version              : 1.527
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Tue Jan 16 16:52:08 2018
+ * C++ source code generated on : Tue Jan 16 17:09:25 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
@@ -280,8 +280,8 @@ void lqr_hoveringModelClass::step()
     /* '<S1>:1:5' y = 0; */
     y = 0;
 
-    /* '<S1>:1:6' z = 0.75; */
-    rtb_Clock = 0.75;
+    /* '<S1>:1:6' z = 0.5; */
+    rtb_Clock = 0.5;
 
     /* '<S1>:1:7' psi = 0; */
   } else if (rtb_Clock <= 30.0) {
@@ -292,8 +292,8 @@ void lqr_hoveringModelClass::step()
     /* '<S1>:1:10' y = 0; */
     y = 0;
 
-    /* '<S1>:1:11' z = 0.75; */
-    rtb_Clock = 0.75;
+    /* '<S1>:1:11' z = 0.5; */
+    rtb_Clock = 0.5;
 
     /* '<S1>:1:12' psi = 0; */
   } else if (rtb_Clock <= 45.0) {
@@ -304,8 +304,8 @@ void lqr_hoveringModelClass::step()
     /* '<S1>:1:15' y = 1; */
     y = 1;
 
-    /* '<S1>:1:16' z = 0.75; */
-    rtb_Clock = 0.75;
+    /* '<S1>:1:16' z = 0.5; */
+    rtb_Clock = 0.5;
 
     /* '<S1>:1:17' psi = 0; */
   } else {
