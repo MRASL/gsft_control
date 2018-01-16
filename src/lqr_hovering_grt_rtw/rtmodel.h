5079976f26b91c192759f42bd1599a0067b888bc
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-15 22:18:36 -0500

    last psi

--- src/lqr_hovering_grt_rtw/rtmodel.h
+++ src/lqr_hovering_grt_rtw/rtmodel.h
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.520
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Mon Jan 15 22:06:18 2018
+ * C++ source code generated on : Mon Jan 15 22:18:07 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
