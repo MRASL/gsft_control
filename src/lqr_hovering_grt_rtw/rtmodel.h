d091384fecaee4e975d61be771f618ddafda2f2a
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-22 22:59:33 -0500

    aa

--- src/lqr_hovering_grt_rtw/rtmodel.h
+++ src/lqr_hovering_grt_rtw/rtmodel.h
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.528
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Mon Jan 22 22:45:25 2018
+ * C++ source code generated on : Mon Jan 22 22:59:11 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
