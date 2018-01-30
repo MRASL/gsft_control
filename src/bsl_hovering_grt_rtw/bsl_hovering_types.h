fe992a7a3c3947e2d1544287b0c4cc81a6df7695
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-01-30 13:06:41 -0500

    lqr_scene21

--- src/bsl_hovering_grt_rtw/bsl_hovering_types.h
+++ src/bsl_hovering_grt_rtw/bsl_hovering_types.h
@@ -7,9 +7,9 @@
  *
  * Code generation for model "bsl_hovering".
  *
- * Model version              : 1.615
+ * Model version              : 1.617
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Tue Jan 30 12:59:10 2018
+ * C++ source code generated on : Tue Jan 30 13:06:14 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
