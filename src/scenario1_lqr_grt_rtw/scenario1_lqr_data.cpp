e409aa7953e306ef3dcdf5001035e82d3d17c5c6
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-02-05 22:21:12 -0500

    ag

---
+++ src/scenario1_lqr_grt_rtw/scenario1_lqr_data.cpp
@@ -0,0 +1,37 @@
+/*
+ * scenario1_lqr_data.cpp
+ *
+ * Student License - for use by students to meet course requirements and
+ * perform academic research at degree granting institutions only.  Not
+ * for government, commercial, or other organizational use.
+ *
+ * Code generation for model "scenario1_lqr".
+ *
+ * Model version              : 1.683
+ * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
+ * C++ source code generated on : Mon Feb  5 22:19:42 2018
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objective: Execution efficiency
+ * Validation result: Passed (1), Warnings (3), Error (0)
+ */
+
+#include "scenario1_lqr.h"
+#include "scenario1_lqr_private.h"
+
+/* Constant parameters (auto storage) */
+const ConstP_scenario1_lqr_T scenario1_lqr_ConstP = {
+  /* Expression: B_ENU_inv
+   * Referenced by: '<Root>/                 '
+   */
+  { 0.16666666666666674, 0.16666666666666669, 0.16666666666666671,
+    0.16666666666666671, 0.16666666666666674, 0.16666666666666671,
+    0.77519379844962388, 1.5503875968992147, 0.77519379844962344,
+    -0.77519379844962344, -1.5503875968992149, -0.77519379844962344,
+    -1.3426750446270113, -2.5529533974250051E-14, 1.3426750446270619,
+    1.3426750446270113, 2.5418511671787539E-14, -1.3426750446270626,
+    -4.5662100456621, 4.5662100456621, -4.5662100456620989, 4.5662100456620971,
+    -4.5662100456621006, 4.5662100456620989 }
+};
