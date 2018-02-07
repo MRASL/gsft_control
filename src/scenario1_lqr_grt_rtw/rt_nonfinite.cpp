86749849d97c5ac0bbe62b1c6590d389c0e95569
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-02-07 15:18:26 -0500

    test2

--- src/scenario1_lqr_grt_rtw/rt_nonfinite.cpp
+++ src/scenario1_lqr_grt_rtw/rt_nonfinite.cpp
@@ -7,9 +7,9 @@
  *
  * Code generation for model "scenario1_lqr".
  *
- * Model version              : 1.712
+ * Model version              : 1.713
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Wed Feb  7 15:12:19 2018
+ * C++ source code generated on : Wed Feb  7 15:17:40 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
