80c19e6db5214624a66f52bcec64a9ff505a5d59
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-02-07 14:13:24 -0500

    new kt

--- src/scenario1_lqr_grt_rtw/rtwtypes.h
+++ src/scenario1_lqr_grt_rtw/rtwtypes.h
@@ -7,9 +7,9 @@
  *
  * Code generation for model "scenario1_lqr".
  *
- * Model version              : 1.702
+ * Model version              : 1.703
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Wed Feb  7 14:01:39 2018
+ * C++ source code generated on : Wed Feb  7 14:13:01 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
