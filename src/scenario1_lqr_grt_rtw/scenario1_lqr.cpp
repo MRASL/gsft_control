8cb6e3d0b193647f7e8813c605f8cc6a5c9513dc
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-02-09 15:04:12 -0500

    again

--- src/scenario1_lqr_grt_rtw/scenario1_lqr.cpp
+++ src/scenario1_lqr_grt_rtw/scenario1_lqr.cpp
@@ -7,9 +7,9 @@
  *
  * Code generation for model "scenario1_lqr".
  *
- * Model version              : 1.716
+ * Model version              : 1.717
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Fri Feb  9 14:58:56 2018
+ * C++ source code generated on : Fri Feb  9 15:03:58 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
@@ -415,8 +415,8 @@ void scenario1_lqrModelClass::step()
       /* '<S3>:1:5' y = X0(2); */
       rtb_ixj = scenario1_lqr_U.X0[1];
 
-      /* '<S3>:1:6' z = 0.75; */
-      rtb_kxi = 0.75;
+      /* '<S3>:1:6' z = 0.5; */
+      rtb_kxi = 0.5;
 
       /* '<S3>:1:7' psi = X0(4); */
       rtb_jxk = scenario1_lqr_U.X0[3];
