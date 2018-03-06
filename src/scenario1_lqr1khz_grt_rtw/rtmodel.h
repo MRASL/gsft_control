a6cbbc07dde301b68cac60d77d47746a93b84b05
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-03-06 13:43:53 -0500

    Catherine last test

--- src/scenario1_lqr1khz_grt_rtw/rtmodel.h
+++ src/scenario1_lqr1khz_grt_rtw/rtmodel.h
@@ -7,9 +7,9 @@
  *
  * Code generation for model "scenario1_lqr1khz".
  *
- * Model version              : 1.777
+ * Model version              : 1.778
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Tue Mar  6 13:36:44 2018
+ * C++ source code generated on : Tue Mar  6 13:43:26 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
--- src/scenario1_lqr1khz_grt_rtw/scenario1_lqr1khz.h
+++ src/scenario1_lqr1khz_grt_rtw/scenario1_lqr1khz.h
@@ -7,9 +7,9 @@
  *
  * Code generation for model "scenario1_lqr1khz".
  *
- * Model version              : 1.777
+ * Model version              : 1.778
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Tue Mar  6 13:36:44 2018
+ * C++ source code generated on : Tue Mar  6 13:43:26 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
@@ -169,7 +169,7 @@ typedef struct {
   real_T RateTransition2;              /* '<Root>/Rate Transition2' */
   real_T psi_e;                        /* '<S3>/psi_e' */
   real_T xe;                           /* '<S5>/xe' */
-  real_T ye;                           /* '<S6>/ye' */
+  real_T Sum3;                         /* '<S6>/Sum3' */
   real_T ze;                           /* '<S7>/ze' */
 } B_scenario1_lqr1khz_T;
 
