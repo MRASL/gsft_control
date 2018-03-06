bfc3aac9e4d89b660f8d6f4e5bb4837ba13ccd43
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-03-05 19:51:42 -0500

    200H_200Hz

--- src/scenario1_lqr1khz_grt_rtw/rtwtypes.h
+++ src/scenario1_lqr1khz_grt_rtw/rtwtypes.h
@@ -7,9 +7,9 @@
  *
  * Code generation for model "scenario1_lqr1khz".
  *
- * Model version              : 1.768
+ * Model version              : 1.769
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Mon Mar  5 19:38:56 2018
+ * C++ source code generated on : Mon Mar  5 19:51:09 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
