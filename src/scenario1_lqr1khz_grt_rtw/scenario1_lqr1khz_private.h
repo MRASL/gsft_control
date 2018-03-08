aef12d8587bef7af48eff807f36e4fe34e7f8b56
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-03-08 12:24:35 -0500

    tunnign2 bounded 25Hz

--- src/scenario1_lqr1khz_grt_rtw/scenario1_lqr1khz_private.h
+++ src/scenario1_lqr1khz_grt_rtw/scenario1_lqr1khz_private.h
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.856
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Thu Mar  8 11:54:55 2018
+ * C++ source code generated on : Thu Mar  8 12:24:09 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
