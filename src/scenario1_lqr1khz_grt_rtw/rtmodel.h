2a74a410bae5b3711071aae0a2755c55107a740e
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-03-08 19:24:26 -0500

    test34

--- src/scenario1_lqr1khz_grt_rtw/rtmodel.h
+++ src/scenario1_lqr1khz_grt_rtw/rtmodel.h
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.858
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Thu Mar  8 19:14:19 2018
+ * C++ source code generated on : Thu Mar  8 19:23:59 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
