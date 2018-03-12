fe05b8a15858c9fb9141c07c22a04410bc5e8491
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-03-12 15:20:59 -0400

    param2_tunning1

--- src/scenario1_lqr1khz_grt_rtw/rtmodel.h
+++ src/scenario1_lqr1khz_grt_rtw/rtmodel.h
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.866
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Mon Mar 12 15:14:32 2018
+ * C++ source code generated on : Mon Mar 12 15:20:42 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
--- src/scenario1_lqr1khz_grt_rtw/scenario1_lqr1khz.cpp
+++ src/scenario1_lqr1khz_grt_rtw/scenario1_lqr1khz.cpp
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.866
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Mon Mar 12 15:14:32 2018
+ * C++ source code generated on : Mon Mar 12 15:20:42 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
@@ -371,7 +371,7 @@ void scenario1_lqr1khzModelClass::step()
     /* Sqrt: '<Root>/Sqrt1' incorporates:
      *  Gain: '<Root>/      '
      */
-    rtb_Sqrt1 = std::sqrt(149253.73134328358 * u0);
+    rtb_Sqrt1 = std::sqrt(116978.4923343994 * u0);
 
     /* Gain: '<Root>/rads_to_RPM' */
     rtb_rads_to_RPM = 9.5493 * rtb_Sqrt1;
