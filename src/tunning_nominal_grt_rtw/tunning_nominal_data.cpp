0b9ce3729c903c7692e2c018ed88fc191763e102
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-06-27 18:08:13 -0400

    file

---
+++ src/tunning_nominal_grt_rtw/tunning_nominal_data.cpp
@@ -0,0 +1,37 @@
+/*
+ * tunning_nominal_data.cpp
+ *
+ * Student License - for use by students to meet course requirements and
+ * perform academic research at degree granting institutions only.  Not
+ * for government, commercial, or other organizational use.
+ *
+ * Code generation for model "tunning_nominal".
+ *
+ * Model version              : 1.1214
+ * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
+ * C++ source code generated on : Wed Jun 27 17:53:25 2018
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objective: Execution efficiency
+ * Validation result: Passed (1), Warnings (3), Error (0)
+ */
+
+#include "tunning_nominal.h"
+#include "tunning_nominal_private.h"
+
+/* Constant parameters (auto storage) */
+const ConstP_tunning_nominal_T tunning_nominal_ConstP = {
+  /* Expression: B_ENU_inv
+   * Referenced by: '<Root>/Control Allocation'
+   */
+  { 0.16666666666666674, 0.16666666666666669, 0.16666666666666671,
+    0.16666666666666671, 0.16666666666666674, 0.16666666666666671,
+    0.77519379844962388, 1.5503875968992147, 0.77519379844962344,
+    -0.77519379844962344, -1.5503875968992149, -0.77519379844962344,
+    -1.3426750446270113, -2.5529533974250051E-14, 1.3426750446270619,
+    1.3426750446270113, 2.5418511671787539E-14, -1.3426750446270626,
+    -4.5662100456621, 4.5662100456621, -4.5662100456620989, 4.5662100456620971,
+    -4.5662100456621006, 4.5662100456620989 }
+};
