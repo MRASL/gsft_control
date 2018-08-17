9a306ffdfb1b8a07f5c16f96a592e22ee44f46a9
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-08-17 16:07:08 -0400

    formule0f

--- src/tunning_nominal_grt_rtw/tunning_nominal_private.h
+++ src/tunning_nominal_grt_rtw/tunning_nominal_private.h
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.1480
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Fri Aug 17 14:23:53 2018
+ * C++ source code generated on : Fri Aug 17 16:06:51 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
