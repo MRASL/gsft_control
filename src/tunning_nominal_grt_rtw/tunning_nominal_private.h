cfa64a2780d10696d8ab67c9ffb1933c1761449c
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-08-28 16:46:28 -0400

    ode4 100Hz

--- src/tunning_nominal_grt_rtw/tunning_nominal_private.h
+++ src/tunning_nominal_grt_rtw/tunning_nominal_private.h
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.1492
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Tue Aug 28 16:24:08 2018
+ * C++ source code generated on : Tue Aug 28 16:46:12 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
