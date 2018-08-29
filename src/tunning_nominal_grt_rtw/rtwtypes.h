71b90db3a0c13f9a5bae4071f85ff051f6a8c22a
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-08-29 10:14:12 -0400

    finalFDD

--- src/tunning_nominal_grt_rtw/rtwtypes.h
+++ src/tunning_nominal_grt_rtw/rtwtypes.h
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.1493
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Tue Aug 28 17:24:17 2018
+ * C++ source code generated on : Wed Aug 29 10:13:44 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
