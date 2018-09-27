f25cda984869d7a1a48e4281d5c3945908f11dc5
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-09-27 14:28:15 -0400

    verify fdd

--- src/tuning_nominal_node.cpp
+++ src/tuning_nominal_node.cpp
@@ -178,8 +178,7 @@ void OdometryCallback(const nav_msgs::OdometryConstPtr& odom_msg) {
 // ASCTEC test
 void MotorSpeedCallback(const asctec_hl_comm::MotorSpeedConstPtr& motor_msg) {
   gsft_control::commandMsg2Thrust(motor_msg,&gThrust_measure);
-  ROS_INFO("Thrust T1 = %f(N)",gThrust_measure[0]);
- // new measurement
+  //ROS_INFO("Thrust T1 = %f(N)",gThrust_measure[0]);
 }
 
 // Gazebo
@@ -448,10 +447,10 @@ int main(int argc, char** argv) {
       gsft_control::LOEPtr LOE_msg(new gsft_control::LOE);
       for(unsigned int i=0; i< 6; i++) {
         if (gThrust_prev_sent[i]!=0.0){
-          LOE_msg->LOE_true[i]= gController.tuning_nominal_Y.LOE_true[i];   // use push_back if array size not fixed
+        //  LOE_msg->LOE_true[i]= gController.tuning_nominal_Y.LOE_true[i];
+          LOE_msg->LOE_true[i]= 1-gThrust_measure[i]/gController.tuning_nominal_Y.thrust_pre[i];
           LOE_msg->LOE_calcul[i] = 1-gThrust_measure[i]/gThrust_prev_sent[i];
-        //  LOE_msg->LOE_calcul[i] = 1-gThrust_measure[i]/gController.tuning_nominal_Y.thrust_pre[i];
-          if (i<3){
+         if (i<3){
              LOE_msg->LOE_FDD[i] = gController.tuning_nominal_Y.LOE13_estimated[i];
           }
         }
