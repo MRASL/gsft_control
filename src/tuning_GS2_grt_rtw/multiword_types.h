4d3796ebd99e77d68860fca69cd1ba809ccfab46
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-12-10 18:47:15 -0500

    gs2 choice 2

--- src/tuning_GS2_grt_rtw/multiword_types.h
+++ src/tuning_GS2_grt_rtw/multiword_types.h
@@ -7,9 +7,9 @@
  *
  * Code generation for model "tuning_GS2".
  *
- * Model version              : 1.2364
+ * Model version              : 1.2369
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Tue Oct 30 09:47:24 2018
+ * C++ source code generated on : Mon Dec 10 18:45:45 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
