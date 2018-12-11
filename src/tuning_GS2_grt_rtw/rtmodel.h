e2a24e404dbea9260f06f16c33d007f537e267c6
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2018-12-10 19:19:08 -0500

    choice 1 gs2 best

--- src/tuning_GS2_grt_rtw/rtmodel.h
+++ src/tuning_GS2_grt_rtw/rtmodel.h
@@ -7,9 +7,9 @@
  *
  * Code generation for model "tuning_GS2".
  *
- * Model version              : 1.2370
+ * Model version              : 1.2374
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Mon Dec 10 19:00:19 2018
+ * C++ source code generated on : Mon Dec 10 19:17:55 2018
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
