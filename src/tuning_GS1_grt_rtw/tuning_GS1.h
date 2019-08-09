d04d714bea14ce6462b9169f7651cec1ad7243d2
Duc-Tien Nguyen <tnguyenduc86@gmail.com>
2019-08-09 10:16:20 -0400

    z, psi, y

--- src/tuning_GS1_grt_rtw/tuning_GS1.h
+++ src/tuning_GS1_grt_rtw/tuning_GS1.h
@@ -7,9 +7,9 @@
  *
  * Code generation for model "tuning_GS1".
  *
- * Model version              : 1.2441
+ * Model version              : 1.2443
  * Simulink Coder version : 8.12 (R2017a) 16-Feb-2017
- * C++ source code generated on : Fri Aug  9 09:42:50 2019
+ * C++ source code generated on : Fri Aug  9 10:10:21 2019
  *
  * Target selection: grt.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
